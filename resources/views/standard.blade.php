023a34b3046b074e62dedf896e511c517dd7bcb0
leticiamasquietto <aplleticia@gmail.com>
2026-04-05 18:21:15 -0300

    Melhorias visuais no projeto
    Tema de Páscoa, ajustes de layout e footer fixo

--- resources/views/standard.blade.php
+++ resources/views/standard.blade.php
@@ -7,7 +7,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 	<title>@yield('title')</title>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100" style="background-color: #3e2723; color: white;">
 	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="{{ route('home') }}">CoelhoChocolate</a>
@@ -17,11 +17,11 @@
 		</div>
 	</nav>
 
-	<main class="container">
-		<div class="m-5">
-			@yield('content')
-		</div>
-	</main>
+	<main class="container flex-fill mt-4">
+    <div class="mt-5">
+        @yield('content')
+    </div>
+</main>
 
 	<div class="container">
 		<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
@@ -30,6 +30,8 @@
 			<p>Letícia Masquietto de Oliveira Silva - 60300765</p>
 		</footer>
 	</div>
-
+<footer class="text-center p-3 mt-auto" style="background-color: #2d1b18;">
+    Loja de Chocolates de Páscoa - Realizado para a matéria de Dev Web II
+</footer>
 </body>
 </html>
